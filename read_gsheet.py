4be701151a283da2fd5465db955d217ba5e341c9
kokohandoko00 <63644640+kokohandoko00@users.noreply.github.com>
2023-08-22 09:42:00 +0700

    add read gsheet file

---
+++ read_gsheet.py
@@ -0,0 +1,15 @@
+import gspread
+from google.auth import exceptions
+from google.oauth2 import service_account
+
+# Load credentials
+credentials = service_account.Credentials.from_service_account_file("gsheetpostgres-4ba412f0c6f3.json", scopes=["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"])
+client = gspread.Client(auth=credentials)
+
+# Access Google Sheets data
+sheet = client.open("Daftar Lamaran Kerja").sheet1
+data = sheet.get_all_records()
+
+# Print the retrieved data
+print(data)
+
